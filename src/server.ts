c31734b8a253fe0db2832abf0bf85d7e2fa7893e
Thomas Démoulins <tdemoulins@gmail.com>
2023-07-28 01:06:36 +0200

    Add favicon & bump modules (#519)

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { Container, inject } from "inversify"
 import { InversifyExpressServer } from "inversify-express-utils"
+import favicon from "serve-favicon"
 import { BetaSeriesAuthProvider } from "./betaseries/authentication"
 import { Configuration } from "./configuration"
 import { ids, provideSingleton } from "./decorators"
 import { ILogger } from "./logger"
 import { getErrorHandler } from "./middlewares/error"
 import { IProcess } from "./process"
+import path from "path"
 
 @provideSingleton(Server)
 export class Server implements IProcess {
@@ -18,6 +20,7 @@ export class Server implements IProcess {
   start() {
     new InversifyExpressServer(this.container, null, null, null, BetaSeriesAuthProvider)
       .setErrorConfig((app) => app.use(getErrorHandler(this.logger)))
+      .setConfig((app) => app.use(favicon(path.join(__dirname, "..", "images", "favicon.png"))))
       .build()
       .listen(this.configuration.server.port)
       .on("listening", () => {
